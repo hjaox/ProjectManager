553834da6c5dd6456fc5f934d3aef9f81159350d
hjaox <hectorjana.o@gmail.com>
2024-03-04 16:23:00 +0000

    [EDIT] changed GET /api/projects/userID to use mongoDB

--- backend/models/projects.model.js
+++ backend/models/projects.model.js
@@ -1,12 +1,13 @@
-//const db = require('../mongo/connection');
-const format = require('pg-format');
+const UserModel = require("../mongo/models/user.model");
+const { sanitizeFilter } = require('mongoose');
 
 function selectProjectsByUserID(userID) {
-    const queryStr = format(`SELECT * FROM projects WHERE for_owner_id = %L`, [userID]);
+    const formatUserId = sanitizeFilter({_id: userID});
 
-    return db.query(queryStr)
-    .then(({rows}) => {
-        return rows
+    return UserModel.findById(formatUserId, "projects")
+    .setOptions({sanitizeFilter: true})
+    .then(res => {
+        console.log(res)
     })
 }
 
