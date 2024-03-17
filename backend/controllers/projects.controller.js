b51306fff1e46737093642ae330a8a792edcb453
hjaox <hectorjana.o@gmail.com>
2024-03-17 16:06:07 +0000

    [EDIT] changed structure of return data of DELETE /api/projects/:userId/:projectId

--- backend/controllers/projects.controller.js
+++ backend/controllers/projects.controller.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 const { findProjectsByUserId,
     deleteProjectById } = require('../models/projects.model');
 
-function getProjectsByUserId(request , response, next) {
+function getProjectsByUserId(request, response, next) {
     const { userId } = request.params;
 
-    if(!mongoose.isValidObjectId(userId)) return response.status(400).send({msg: "Invalid UserId"});
+    if (!mongoose.isValidObjectId(userId)) return response.status(400).send({ msg: "Invalid UserId" });
 
     return findProjectsByUserId(userId)
-    .then(projectsDataByUserIDResult => {
-        return response.status(200).send({projects: projectsDataByUserIDResult});
-    })
-    .catch(err => {
-        next(err)
-    })
+        .then(projectList => {
+            return response.status(200).send({ projects: projectList });
+        })
+        .catch(err => {
+            next(err)
+        })
 }
 
 function removeProject(request, response, next) {
@@ -22,8 +22,8 @@ function removeProject(request, response, next) {
     if (!mongoose.isValidObjectId(userId) || !mongoose.isValidObjectId(projectId)) return response.status(400).send({ msg: "Invalid userId, projectId or columnId" });
 
     return deleteProjectById(userId, projectId)
-        .then(projectDetails => {
-            return response.status(202).send({ projectDetails })
+        .then(([projects]) => {
+            return response.status(202).send({ projects })
         })
         .catch(err => {
             next(err)
--- backend/models/projects.model.js
+++ backend/models/projects.model.js
@@ -2,15 +2,15 @@ const UserModel = require("../mongo/models/user.model");
 const { sanitizeFilter } = require('mongoose');
 
 function findProjectsByUserId(userId) {
-    const formatQuery = sanitizeFilter({_id: userId});
+    const formatQuery = sanitizeFilter({ _id: userId });
 
     return UserModel.findById(formatQuery, "projects")
-    .then(({projects}) => {
-        return projects
-    })
-    .catch(() => {
-        return Promise.reject({status: 404, msg: "Not Found"});
-    })
+        .then(({ projects }) => {
+            return projects
+        })
+        .catch(() => {
+            return Promise.reject({ status: 404, msg: "Not Found" });
+        })
 }
 
 const deleteProjectById = async (userId, projectId) => {
@@ -22,7 +22,7 @@ const deleteProjectById = async (userId, projectId) => {
         doc = await UserModel.findById(formatQuery);
         doc.projects.id(formatProjId).deleteOne();
         await doc.save();
-        return doc;
+        return doc.projects;
     } catch (err) {
         return Promise.reject({ status: 404, msg: "UserId or ProjectId not found" })
     }
