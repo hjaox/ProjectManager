92f3d87b957ff6b024034ea83fda411f072fb3bb
hjaox <hectorjana.o@gmail.com>
2024-04-28 19:52:18 +0100

    [STYLE] added style to Login component

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -3,14 +3,16 @@ import { useNavigate } from "react-router-dom";
 import loginUser from "../../utils/axios/login";
 import { useDispatch } from "react-redux";
 import { actions } from "../../utils/redux/reducers";
+import "../../style/Login/login.scss";
 
 export default function Login() {
-    const [username, setUsername] = useState("");
+    const [email, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const [loginError, setLoginError] = useState(false);
 
-    function handleUsernameInput(event: React.ChangeEvent<HTMLInputElement>): void {
+    function handleEmailInput(event: React.ChangeEvent<HTMLInputElement>): void {
         setUsername(event.target.value);
     }
 
@@ -18,29 +20,41 @@ export default function Login() {
         setPassword(event.target.value);
     }
 
-    function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
+    async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
 
-        loginUser(username, password)
-            .then(loginDetails => {
-                dispatch(actions.login(loginDetails))
-                navigate(`/Dashboard`)
-            })
-            .catch(err => {
-                console.log(err)
-            })
+        try {
+            const loginDetails = await loginUser(email, password);
+            dispatch(actions.login(loginDetails));
+            navigate("/Dashboard");
+        } catch {
+            setLoginError(true)
+        }
     }
 
     return (
-        <form id="loginForm"
-            onSubmit={event => handleSubmit(event)}>
-            <label htmlFor="usernameInput">Name:</label>
-            <input className="border border-black" name="usernameInput" id="usernameInput" type="text" onChange={event => handleUsernameInput(event)} />
+        <section className="login-page">
+            <div className="login-display">
+                <div className="login-title">
+                    <div onClick={() => navigate("/Home")} className="title-projectname">Project Manager</div>
+                    <div className="title-login">Login</div>
+                    <div className={`title-error ${loginError ? "show-error" : "hide-error"}`}>Incorrect email or password</div>
+                </div>
 
-            <label htmlFor="usernameInput">Email:</label>
-            <input className="border border-black" name="passwordInput" id="passwordInput" type="text" onChange={event => handlePasswordInput(event)} />
+                <form id="login-form" onSubmit={event => handleSubmit(event)}>
+                    <div className="form-input">
+                        <label htmlFor="input-email" className={`${loginError ? "error-label" : ""}`}>Email:</label>
+                        <input className="border border-black" name="input-email" id="input-email" type="text" onChange={event => handleEmailInput(event)} />
+                    </div>
 
-            <button type="submit" form="loginForm">Login</button>
-        </form>
+                    <div className="form-input">
+                        <label htmlFor="input-password" className={`${loginError ? "error-label" : ""}`}>Password:</label>
+                        <input className="border border-black" name="input-password" id="input-password" type="text" onChange={event => handlePasswordInput(event)} />
+                    </div>
+
+                    <button type="submit" form="login-form">Login</button>
+                </form>
+            </div>
+        </section>
     )
 }
