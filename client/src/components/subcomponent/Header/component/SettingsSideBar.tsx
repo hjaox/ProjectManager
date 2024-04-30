1c162eececd36f060e392e7c224e4f274b61ce1f
hjaox <hectorjana.o@gmail.com>
2024-04-30 18:09:14 +0100

    [STYLE] added styling to SettingsSideBar component for Header

--- client/src/components/subcomponent/Header/component/SettingsSideBar.tsx
+++ client/src/components/subcomponent/Header/component/SettingsSideBar.tsx
@@ -1,35 +1,57 @@
 import { useSelector } from "react-redux"
 import { ProfileState } from "../../../../common/types";
 import { useState } from "react";
+import "../../../../style/subcomponents/settingsSideBar.scss";
 
 export default function SettingsSideBar() {
     const userDetails = useSelector((state: ProfileState) => state.userDetails);
     const [showSettings, setShowSettings] = useState(false);
 
     function handleName(name: string) {
-        if(!name) return <>Guest</>;
+        if (!name) return <>Guest</>;
 
         const splitName = name.split(" ");
         const formatName = splitName.length === 1
-        ? `${splitName[0][0].toUpperCase()}`
-        : `${splitName[0][0].toUpperCase()}${splitName[splitName.length - 1][0].toUpperCase()}`;
+            ? `${splitName[0][0].toUpperCase()}`
+            : `${splitName[0][0].toUpperCase()}${splitName[splitName.length - 1][0].toUpperCase()}`;
 
         return (
-        <>{formatName}</>
+            <>{formatName}</>
         );
     }
 
     return (
-        <div id="settings-icon" className="w-36 relative flex items-center justify-end">
-            <div className="border-solid border-black rounded-full w-10 h-10 bg-slate-500 mr-1 justify-self-end flex place-items-center place-content-center"
-            onClick={() => setShowSettings(() => !showSettings)}>
+        <section className="settings">
+            <div className="settings-user">
                 <span>{handleName(userDetails.name)}</span>
             </div>
 
-            <div id="settings-options" className={`flex flex-col absolute right-0 top-12 mr-1 ${!showSettings && "hidden"}`}>
-                <span>settings</span>
-                <span>settings</span>
+            <div className="settings-icon" onClick={() => setShowSettings(() => !showSettings)}>
+                {
+                    showSettings
+                        ? (
+                            <div className="settings-open">
+                                <div className="settings-open-line slash"></div>
+                                <div className="settings-open-line backslash"></div>
+                            </div>
+                        )
+                        : (
+                            <div className="settings-closed">
+                                <div className="settings-closed-line"></div>
+                                <div className="settings-closed-line"></div>
+                                <div className="settings-closed-line"></div>
+                            </div>
+                        )
+                }
+
             </div>
-        </div>
+            <div className={`settings-dropdown ${showSettings ? "dropdown-show" : "dropdown-hide"}`}>
+                <div className="settings-dropdown-option">settings</div>
+                <div className="settings-dropdown-option">settings</div>
+                <div className="settings-dropdown-option">settings</div>
+                <div className="settings-dropdown-option">settings</div>
+                <div className="settings-dropdown-option">settings</div>
+            </div>
+        </section>
     )
 }
