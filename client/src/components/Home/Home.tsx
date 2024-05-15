6e9baf5cd23a99ebbed5ab06a3f0c5c399554c45
hjaox <hectorjana.o@gmail.com>
2024-05-15 15:24:30 +0100

    [FEAT] created loginAsGuest in Home component

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,9 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import Footer from "../subcomponent/Footer.tsx/Footer";
 import "../../style/Home/home.scss";
+import { useDispatch } from "react-redux";
+import { actions } from "../../utils/redux/reducers";
 
 export default function Home() {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    function handleGuest() {
+        dispatch(actions.loginAsGuest());
+        navigate("/Dashboard")
+    }
 
     return (
         <section className="home-layout">
@@ -12,7 +20,7 @@ export default function Home() {
                 <div className="home-header-navigation">
                     <button className="home-header-navigation-option" onClick={() => navigate("/Login")}>Login</button>
                     <button className="home-header-navigation-option" onClick={() => navigate("/Register")}>Regsiter</button>
-                    <button className="home-header-navigation-option">Try it out as Guest</button>
+                    <button className="home-header-navigation-option" onClick={() => handleGuest()}>Try it out as Guest</button>
                 </div>
             </header>
 
